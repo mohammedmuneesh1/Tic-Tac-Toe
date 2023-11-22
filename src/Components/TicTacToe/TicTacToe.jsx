f134321994436f16d828c934a0a94948e94b9653
mohammed muneesh E K <mohammedmuneesh1@gmail.com>
2023-11-22 18:03:00 +0530

    Reset-Part-Pending

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -11,6 +11,12 @@ export const TicTacToe = () => {
     let [count,setCount] = useState(0);
     let [lock,setLock] = useState(false);
     let titleRef = useRef(null);
+    // let b1,b2,b3,b4,b5,b6,b7,b8,b9 = useRef(null); 
+    // let [b1, b2, b3, b4, b5, b6, b7, b8, b9] = Array.from({ length: 9 }, () => useRef(null));
+    // const [b1, b2, b3, b4, b5, b6, b7, b8, b9] = Array.from({ length: 9 }, () => useRef(null));
+    // const refs = Array.from({ length: 9 }, () => useRef(null));
+
+    const brr = [b1,b2,b3,b4,b5,b6,b7,b8,b9]
 
     const toggle = (e,num)=>{
         if(lock) return 0;
@@ -29,37 +35,37 @@ export const TicTacToe = () => {
 
     const chkWin = ()=>{
         if(data[0] === data[1] && data[1] === data[2] && data[2]!==""){
-         won(data);
+         won(data[2]);
         }
         else if (data[3] === data[4] && data[4] === data[5] && data[5]!==""){
-         won(data);
+         won(data[5]);
 
         }
         else if (data[6] === data[7] && data[7] === data[8] && data[8]!==""){
-            won(data);
+            won(data[8]);
    
            }
            else if (data[0] === data[3] && data[3] === data[6] && data[6]!==""){
-            won(data);
+            won(data[6]);
    
            }
            else if (data[1] === data[4] && data[4] === data[7] && data[7]!==""){
-            won(data);
+            won(data[7]);
    
            }
            else if (data[2] === data[5] && data[5] === data[8] && data[8]!==""){
-            won(data);
+            won(data[8]);
    
            }
            else if (data[0] === data[4] && data[4] === data[8] && data[8]!==""){
-            won(data);
+            won(data[8]);
 
            }
            else if (data[0] === data[1] && data[1] === data[2] && data[2]!==""){
-            won(data);
+            won(data[2]);
            }
            else if (data[2] === data[4] && data[4] === data[6] && data[6]!==""){
-            won(data);
+            won(data[6]);
            }
 
 
@@ -71,9 +77,19 @@ export const TicTacToe = () => {
             titleRef.current.innerHTML = `Congratulations: <img src='${cross}'> Won`;
         }
         else{
-            titleRef.current.innerHTML =  `Congragulations:<img src='${circle}'`
+            titleRef.current.innerHTML =  `Congragulations: <img src='${circle}'> Won`
         }
 
+    
+    }
+
+    const reset = ()=>{
+        setLock(false);
+         data = ["","","","","","","","",""];
+        titleRef.current.innerHTML =`Tic Tac Toe Game In <span>React</span>`
+        brr.map(value=>{
+            value.current.innerHTML = ""
+        })
     
     }
 
@@ -81,27 +97,27 @@ export const TicTacToe = () => {
 
   return (
     <div className='container'>
-        <h1 className='title' ref={titleRef}> Tic Tac Toe Game in <span>REACT</span></h1>
+        <h1 className='title' ref={titleRef}> Tic Tac Toe Game In <span>REACT</span></h1>
      <div className='board'>
    <div className="row1">
-    <div className="boxes" onClick={(e)=>toggle(e,0)}></div>
-    <div className="boxes"onClick={(e)=>toggle(e,1)}></div>
-    <div className="boxes" onClick={(e)=>toggle(e,2)}></div>
+    <div className="boxes" onClick={(e)=>toggle(e,0)}       ref={b1} ></div> 
+    <div className="boxes"onClick={(e)=>toggle(e,1)}         ref={b2}></div>
+    <div className="boxes" onClick={(e)=>toggle(e,2)}        ref={b3} ></div>
    </div>
    <div className="row2">
-    <div className="boxes" onClick={(e)=>toggle(e,3)}></div>
-    <div className="boxes" onClick={(e)=>toggle(e,4)}></div>
-    <div className="boxes"  onClick={(e)=>toggle(e,5)}></div>
+    <div className="boxes" onClick={(e)=>toggle(e,3)} ref={b4}></div>
+    <div className="boxes" onClick={(e)=>toggle(e,4)} ref={b5}></div>
+    <div className="boxes"  onClick={(e)=>toggle(e,5)}  ref={b6}></div>
    </div>
    <div className="row3">
-    <div className="boxes"  onClick={(e)=>toggle(e,6)}></div>
-    <div className="boxes" onClick={(e)=>toggle(e,7)}></div>
-    <div className="boxes" onClick={(e)=>toggle(e,8)}></div>
+    <div className="boxes"  onClick={(e)=>toggle(e,6)} ref={b7}></div>
+    <div className="boxes" onClick={(e)=>toggle(e,7)}   ref={b8}></div>
+    <div className="boxes" onClick={(e)=>toggle(e,8)} ref={b9}></div>
    </div>
      </div>
      
 
-     <button className='reset'>Reset</button>
+     <button className='reset' onClick={()=>reset()} >Reset</button>
     </div>
   )
 }
